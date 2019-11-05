77cb593184d224822eaba70360df6d01687d78b6
Paul Martensen <paul.martensen@gmx.de>
2019-11-05 20:05:27 +0100

    Added basic sim module.

---
+++ src/sim/player.rs
@@ -0,0 +1,51 @@
+use super::game::Game;
+use super::*;
+use crate::util::*;
+
+#[derive(Debug)]
+pub struct Player {
+    transform: Transform,
+}
+
+impl Player {
+    const STD_VEL: [D; 3] = [10., 10., 30.];
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        let transform = Transform {
+            pos: [0., 0., 0.].into(),
+            vel: [0., 0., Self::STD_VEL[2]].into(),
+            scale: 1.,
+        };
+        Player { transform }
+    }
+}
+
+impl Commandable for Player {
+    fn handle(&mut self, c: Command) -> SimResult {
+        match c {
+            Command::Move(dir) => {
+                let scale: Vector = [dir[0], dir[1], 1.].into();
+                let vel = self.transform.vel;
+                self.transform.vel = [
+                    (vel[0] + scale[0] * Self::STD_VEL[0])
+                        .max(Game::LEFT_BOUND)
+                        .min(Game::RIGHT_BOUND),
+                    (vel[1] + scale[1] * Self::STD_VEL[1])
+                        .max(Game::LOWER_BOUND)
+                        .min(Game::UPPER_BOUND),
+                    vel[2],
+                ]
+                .into()
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Updateable for Player {
+    fn update(&mut self, d: Duration) -> SimResult {
+        self.transform.update(d)
+    }
+}
