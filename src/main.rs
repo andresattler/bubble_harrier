6a42c00c9dee325f618909d3eb69f58d183d2112
Paul Martensen <paul.martensen@gmx.de>
2019-11-18 22:18:29 +0100

    Initialized env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+extern crate env_logger;
+extern crate log;
+extern crate nalgebra as na;
+extern crate ncollide3d as nc;
+
 mod bundles;
 mod components;
 mod game;
 mod util;
 
 fn main() {
+    env_logger::init();
     let mut game = game::Game::new();
     game.run()
 }
