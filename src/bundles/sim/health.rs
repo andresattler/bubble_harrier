18d8bc6f81a06ee27f95f45c2d2439505124f696
andresattler <andressattler.webdev@gmail.com>
2019-11-18 21:56:55 +0100

    added some improvements and a basic ui for displaying health

--- src/bundles/sim/health.rs
+++ src/bundles/sim/health.rs
@@ -5,21 +5,22 @@ pub struct HealthSystem;
 
 impl<'s> specs::System<'s> for HealthSystem {
     type SystemData = (
-        ReadStorage<'s, Health>,
+        WriteStorage<'s, Health>,
         ReadStorage<'s, ObjectKind>,
         WriteStorage<'s, Transform>,
         Entities<'s>,
     );
 
-    fn run(&mut self, (healths, kinds, mut transforms, entities): Self::SystemData) {
-        for (health, kind, transform, entity) in
-            (&healths, &kinds, &mut transforms, &entities).join()
+    fn run(&mut self, (mut healths, kinds, mut transforms, entities): Self::SystemData) {
+        for (mut health, kind, transform, entity) in
+            (&mut healths, &kinds, &mut transforms, &entities).join()
         {
-            if health.current == 0 {
+            if health.current <= 0 {
                 match kind {
                     ObjectKind::Player => {
                         let new_pos: Vector = Vector::from([0., 0., 0.]);
                         transform.position = new_pos.into();
+                        health.current = health.full;
                     }
                     ObjectKind::Obstacle => {
                         entities.delete(entity).unwrap();
