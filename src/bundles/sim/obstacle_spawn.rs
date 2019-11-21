0ae891e0b1a2f0351d1441b58b3e36680fa78c5e
andresattler <andressattler.webdev@gmail.com>
2019-11-21 21:40:55 +0100

    moved logic into separated methods

--- src/bundles/sim/obstacle_spawn.rs
+++ src/bundles/sim/obstacle_spawn.rs
@@ -11,6 +11,39 @@ impl ObstacleSpawnSystem {
     pub fn name() -> &'static str {
         "sim::obstacle_spawn_system"
     }
+
+    fn add_obstacle(updater: &LazyUpdate, entities: &Entities, x: f32, z: f32) {
+        let obstacle = entities.create();
+        updater.insert(obstacle, ObjectKind::Obstacle);
+        updater.insert(obstacle, Extent::new(1.));
+        updater.insert(
+            obstacle,
+            Transform3D::<D>::default().with_position([
+                x,
+                0.,
+                z + 200.,
+            ]),
+        );
+        updater.insert(obstacle, Extent::new(1.));
+        updater.insert(
+            obstacle,
+            Health {
+                current: 1,
+                full: 1,
+            },
+        );
+    }
+
+    fn add_row(updater: &LazyUpdate, entities: &Entities, z: f32) {
+        let mut rng = thread_rng();
+        let row_lenght = rng.gen_range(1, 5);
+        for _i in 1..=row_lenght {
+            let rand_x = rng.gen_range(RIGHT_BOUND, LEFT_BOUND);
+            Self::add_obstacle(&updater, &entities, rand_x, z)
+        }
+        
+    }
+
 }
 
 impl<'s> specs::System<'s> for ObstacleSpawnSystem {
@@ -25,30 +58,7 @@ impl<'s> specs::System<'s> for ObstacleSpawnSystem {
         if let Some(ptrans) = transforms.get(player.0) {
             let py_int = ptrans.position[2] as u32;
             if py_int % DISTANCE == 0 {
-                let obstacle = entities.create();
-                updater.insert(obstacle, ObjectKind::Obstacle);
-                updater.insert(obstacle, Extent::new(1.));
-                let mut rng = thread_rng();
-                let row_lenght = rng.gen_range(1, 5);
-                for _i in 1..=row_lenght {
-                    let rand_x = rng.gen_range(RIGHT_BOUND, LEFT_BOUND);
-                    updater.insert(
-                        obstacle,
-                        Transform3D::<D>::default().with_position([
-                            rand_x,
-                            0.,
-                            ptrans.position[2] + 200.,
-                        ]),
-                    );
-                    updater.insert(obstacle, Extent::new(1.));
-                    updater.insert(
-                        obstacle,
-                        Health {
-                            current: 1,
-                            full: 1,
-                        },
-                    );
-                }
+                Self::add_row(&updater, &entities, ptrans.position[2]);
             }
         }
     }
